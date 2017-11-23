8a81bf980c3e21e17271c3c838f7e03e67fb0c3e
duncan heron <duncanuk@gmail.com>
2017-11-23 11:36:33 +0000

    validates the blockchain is no longer valid

--- main.js
+++ main.js
@@ -6,9 +6,60 @@ class Block{
 		this.timestamp = timestamp;
 		this.data = data;
 		this.previousHash = previousHash;
+		this.hash = this.calculateHash()
 	}
 
 	calculateHash() {
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString());
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
 }
+
+class Chain {
+	constructor() {
+		this.chain = [this.createGenesisBlock()];
+	}
+
+	createGenesisBlock() {
+		return new Block(0,"22/12/2017","Genesis Block","0");
+	}
+
+	getLatestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock) {
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock)
+	}
+
+	isChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i -1];
+			if(currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+			if(currentBlock.previousHash !== previousBlock.hash) {
+				return false
+			}
+		}
+		return true
+
+	}
+}
+
+let duncanCoin = new Chain();
+duncanCoin.addBlock(
+	new Block(1, "10/01/2018", {amount: 5})
+);
+duncanCoin.addBlock(
+	new Block(2, "10/03/2018", {amount: 500})
+);
+duncanCoin.addBlock(
+	new Block(3, "10/05/2018", {amount: 5000})
+);
+
+console.log(duncanCoin.isChainValid());
+duncanCoin.chain[2].data = {amount:1000000};
+console.log(duncanCoin.isChainValid());
